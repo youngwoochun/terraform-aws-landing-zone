a6940f7217113f9706191507bbdf6af9e26285c2
Young Chun <youngchun@Youngs-MacBook-Pro.local>
2020-07-01 16:15:47 -0400

    removed ec2 modules

--- product-a/main.tf
+++ product-a/main.tf
@@ -14,18 +14,18 @@ module "network" {
   cluster_name            = var.cluster_name
 }
 
-module "bastion" {
-  source                      = "../ec2"
-  instance_count              = var.bastion_instance_defaults["instance_count"]
-  ami                         = var.bastion_instance_defaults["ami"]
-  instance_type               = var.bastion_instance_defaults["instance_type"]
-  user_data                   = var.bastion_instance_defaults["user_data"]
-  subnet_id                   = module.network.public_subnets
-  key_name                    = var.bastion_instance_defaults["key_name"]
-  monitoring                  = var.bastion_instance_defaults["monitoring"]
-  vpc_security_group_ids      = module.network.bastion_security_group_id
-  iam_instance_profile        = var.bastion_instance_defaults["iam_instance_profile"]
-  associate_public_ip_address = var.bastion_instance_defaults["associate_public_ip_address"]
-  ebs_optimized               = var.bastion_instance_defaults["ebs_optimized"]
-  tags                        = var.tags
-}
+#module "bastion" {
+#  source                      = "../ec2"
+#  instance_count              = var.bastion_instance_defaults["instance_count"]
+#  ami                         = var.bastion_instance_defaults["ami"]
+#  instance_type               = var.bastion_instance_defaults["instance_type"]
+#  user_data                   = var.bastion_instance_defaults["user_data"]
+#  subnet_id                   = module.network.public_subnets
+#  key_name                    = var.bastion_instance_defaults["key_name"]
+#  monitoring                  = var.bastion_instance_defaults["monitoring"]
+#  vpc_security_group_ids      = module.network.bastion_security_group_id
+#  iam_instance_profile        = var.bastion_instance_defaults["iam_instance_profile"]
+#  associate_public_ip_address = var.bastion_instance_defaults["associate_public_ip_address"]
+#  ebs_optimized               = var.bastion_instance_defaults["ebs_optimized"]
+#  tags                        = var.tags
+#}
