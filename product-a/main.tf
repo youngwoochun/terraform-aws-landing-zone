48065bf89d5c84abeb9aff1f31485c7c3a14fced
Young Chun <youngchun@Youngs-MacBook-Pro.local>
2020-07-01 11:03:59 -0400

    testing pipeline

--- product-a/main.tf
+++ product-a/main.tf
@@ -20,10 +20,10 @@ module "bastion" {
   ami                         = var.bastion_instance_defaults["ami"]
   instance_type               = var.bastion_instance_defaults["instance_type"]
   user_data                   = var.bastion_instance_defaults["user_data"]
-  subnet_id                   = data.terraform_remote_state.network
+  subnet_id                   = module.network.public_subnets
   key_name                    = var.bastion_instance_defaults["key_name"]
   monitoring                  = var.bastion_instance_defaults["monitoring"]
-  vpc_security_group_ids      = data.terraform_remote_state.network
+  vpc_security_group_ids      = module.network.bastion_security_group_id
   iam_instance_profile        = var.bastion_instance_defaults["iam_instance_profile"]
   associate_public_ip_address = var.bastion_instance_defaults["associate_public_ip_address"]
   ebs_optimized               = var.bastion_instance_defaults["ebs_optimized"]
