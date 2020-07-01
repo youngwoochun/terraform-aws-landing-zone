d179dcb1443dda9a4adb1b7d4c3d3342ac8a03f8
Young Chun <youngchun@Youngs-MacBook-Pro.local>
2020-07-01 11:05:23 -0400

    testing pipeline

--- product-a/backend.tf
+++ product-a/backend.tf
@@ -6,12 +6,3 @@ terraform {
     region               = "us-east-1"
   }
 }
-
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-state-product-a"
-    key    = "dev/us-east-1/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
