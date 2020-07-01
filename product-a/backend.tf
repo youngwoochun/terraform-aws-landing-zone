690359d3aa7c487fc6803773b345d92633c57af5
Young Chun <youngchun@Youngs-MacBook-Pro.local>
2020-07-01 16:35:07 -0400

    changed the remote state key

--- product-a/backend.tf
+++ product-a/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket               = "terraform-state-product-a"
     dynamodb_table       = "terraform-state-product-a-lock"
-    key                  = "dev/us-east-1/terraform.tfstate"
+    key                  = "dev/us-east-1/landing-zone"
     region               = "us-east-1"
   }
 }
